299dde7cec17e4160676542836bada2ce5acbf4c
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-17 22:20:27 +0900

    7.5 React Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { 
+  BrowserRouter as Router,
+  Switch, // Route 찾는 용도, 한번에 하나의 Route만 렌더링 하기 위해서
+  Route, // url
+  Link, // 브라우저 새로고침 없이도 유저를 다른 페이지로 이동시켜주는 컴포넌트
+  } from "react-router-dom";
+  import Home from "./routes/Home";
+  import Detail from "./routes/Detail";
+
 
 function App() {
   return (
-    null
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/"> {/* component 적는 곳*/}
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
