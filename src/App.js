12c5be7ddc12eeebd4b78cf47ce861a206de7e39
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-17 21:54:25 +0900

    7.4 Movie App part Two :  npm i react-router-dom@5.3.0 설치 및 routes, components 폴더 생성, Detail&Home&Movie 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,51 +1,7 @@
-import { useEffect, useState } from "react";
-
 
 function App() {
-  // loading = data, setLoading = data를 수정할 수 있는 함수
-  // setLoading 함수를 실행하면, component가 다시 render한다.
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async() => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    );
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, [])
-  console.log(movies);
-
-  return(
-    <div>
-      { loading ? (
-        <h1>Loading</h1>
-        ) : (
-          <div>
-            {movies.map((movie) => (
-              <div 
-                key={movies.id}
-              >
-                <img src={movie.medium_cover_image} />
-                <h2>{movie.title}</h2>
-                <p>{movie.summary}</p>
-                <ul>
-                  {movie.genres.map((genre) => (
-                    <li key={genre}>{genre}</li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div>
-
-
-
-        ) }
-    </div>
+  return (
+    null
   );
 }
 
---
+++ src/components/Movie.js
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+function Movie({ mediumCoverImage, title, summary, genres }){
+    return(
+        <div>
+            <img src={mediumCoverImage} alt={title} />
+            <h2>{title}</h2>
+            <p>{summary}</p>
+            <ul>
+                {genres.map((genre) => (
+                <li key={genre}>{genre}</li>
+                ))}
+            </ul>
+        </div>
+
+    );
+}
+
+Movie.propTypes = {
+    mediumCoverImage: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+}
+
+export default Movie;
