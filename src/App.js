78696a1ba67052dca87fca084104a4add7aa6830
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-17 16:40:52 +0900

    7.0 To Do List part One - 1

--- src/App.js
+++ src/App.js
@@ -1,47 +1,32 @@
-import { useState , useEffect } from "react";
-
-function Hello() {
-  function destroyedFn(){
-    console.log("Destroyed :(");
-  }
-
-  function effectFn() {
-    console.log("Created :)");
-    return destroyedFn;
-  }
-
-  // 방법1
-  // useEffect(effectFn, []);
-  // 방법2
-  // useEffect(() => {
-  //   console.log("Created :)");
-  //   return function(){
-  //     console.log("Destroyed :(");
-  //   }
-  // }, []);
-  // 방법3 ( 더 잘쓰이는 방법)
-  useEffect(() => {
-    console.log("Created :)");
-    return () => console.log("Destroyed :(");
-  }, []);
-
-  return (
-    <h1>Hello</h1>
-  );
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => {
+    setToDo(event.target.value);
   }
-  
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // console.log(toDo);
+    if (toDo === ""){
+      return; // toDo에 아무것도 없다면 이 함수는 실행되지 않는다.
+    }
+    setToDo(""); // toDo값을 수정하는 함수
+  }
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show" }</button>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange} 
+          value={toDo} 
+          type="text" 
+          placeholder="Write your to do.." 
+          />
+        <button>Add To Do</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
