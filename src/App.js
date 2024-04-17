c5ba6a516ef931c5e4ea6469589322fd2f581b2d
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-17 17:06:02 +0900

    7.0 To Do List part One - 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { useState } from "react";
 
 function App() {
   const [toDo, setToDo] = useState("");
+
+  // 여러개의 toDo를 받을 수 있는 array 만들기
+  const [toDos, setToDos] = useState([]);
+
   const onChange = (event) => {
     setToDo(event.target.value);
   }
@@ -11,11 +15,20 @@ function App() {
     if (toDo === ""){
       return; // toDo에 아무것도 없다면 이 함수는 실행되지 않는다.
     }
-    setToDo(""); // toDo값을 수정하는 함수
+
+    setToDo(""); // toDo값을 수정하는 함수 => toDo = "" 이렇게 직접 수정X
+    //js이라면 toDos.push()로 toDo를 toDos에 추가해줬을 것이다 (state에서는 직접수정불가)
+    // setToDos(function(currentArray){
+    //   return(
+    //   );
+    // }); // 아래 코드와 같음
+    setToDos((currentArray) => [toDo, ...currentArray]); // toDo를 currentArray에 추가함
   }
+  console.log(toDos);
 
   return (
     <div>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input 
           onChange={onChange} 
