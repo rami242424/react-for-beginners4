d51dccf7c41143dd829f888dc8eb5082259e917a
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-17 10:55:38 +0900

    6.1 useEffect -1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 // 특정 코드들이 첫번째 component render에서만 실행되게 하는 방법
-// ex) 똑같은 API를 렌더링 할때마다(다른 state를 업데이트 할때마다) 계속 불러오는 것 -> 속도가 느려질 수 있는 원인이 된다.
+// =>> useEffect 사용
+// 필요한 이유 ex) 똑같은 API를 렌더링 할때마다(다른 state를 업데이트 할때마다) 계속 불러오는 것 -> 속도가 느려질 수 있는 원인이 된다.
+// useEffect( 딱 한번만 실행하고 싶은 코드, deps?: DependencyList )
 
-import { useState } from "react";
+
+import { useState , useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render");
+  
+  console.log("I run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("I run only once");
+  }
+  useEffect(iRunOnlyOnce, []);
+
   return (
     <div>
       <h1>{counter}</h1>
