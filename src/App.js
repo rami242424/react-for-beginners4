0f32ee6b57c1c55d30ae3956c070eb1bb0915f39
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-17 12:08:28 +0900

    6. clean up : create, destroyed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { useState , useEffect } from "react";
 
 function Hello() {
+  useEffect(() => {
+    console.log("Created :)");
+    return () => 
+      console.log("Destroyed :(");
+  }, []);
+
   return (
     <h1>Hello</h1>
   );
