a53a1e4da85c311ab5642b18a202ad4f57b62419
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-17 11:09:53 +0900

    6.2 Deps _ 클릭을 누를때, keyword도 rerendering 되고 있다 => 불필요한 렌더링

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-// 특정 코드들이 첫번째 component render에서만 실행되게 하는 방법
-// =>> useEffect 사용
-// 필요한 이유 ex) 똑같은 API를 렌더링 할때마다(다른 state를 업데이트 할때마다) 계속 불러오는 것 -> 속도가 느려질 수 있는 원인이 된다.
-// useEffect( 딱 한번만 실행하고 싶은 코드, deps?: DependencyList )
-
-
 import { useState , useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   
-  // useEffect 사용해보기
   console.log("I run all the time");
-  // 표현빙식 1
-  // const iRunOnlyOnce = () => {
-  //   console.log("I run only once");
-  // }
-  // useEffect(iRunOnlyOnce, []);
-
-  // 표현방식 2
   useEffect(() => {
     console.log("CALL the API");
   }, []);
-
-
+  console.log("Search for", keyword);
 
   return (
     <div>
+      <input 
+        value={keyword} 
+        onChange={onChange} 
+        type="text" 
+        placeholder="Search here..." 
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
