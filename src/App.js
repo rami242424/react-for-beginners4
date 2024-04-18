68c9d027fda2bdb46dce04c23b90c295dc58f850
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-18 22:48:45 +0900

    14.2 Building the Navigation: BrouserRouter사용_ 차이점 없음_but 깃헙업로드 불편.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // Link는 라우터 안에 있어야 한다. 따라서 App.js에서 Navigation도 Route 안에 있어야한다.
 
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
@@ -10,12 +10,12 @@ import Navigation from "./components/Navigation";
 function App(){
   return (
     <>
-      <HashRouter>
+      <BrowserRouter>
         <Navigation />
         <Route path="/" component={Home} exact={true}/>
         <Route path="/home/detail" component={Detail} exact={true}/>
         <Route path="/about" component={About} exact={true}/>
-      </HashRouter>
+      </BrowserRouter>
       <footer></footer>
     </>
   );
