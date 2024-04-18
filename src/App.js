7c9369e77fb9a1d103e0e063df8b23f159b53457
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-18 12:56:05 +0900

    10.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React from "react";
 
-function Food({ fav }){ 
-  // console.log(props);
+function Food({ name, image }){
   return (
-    <h2>Food i love {fav}</h2>
+    <div>
+      <h2>i love { name }</h2>
+      <img src={ image } />
+    </div>
   );
 }
+
+const foodILike = [
+  {
+  name: "Kimchi",
+  image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+  },
+  {
+  name: "bulgogi",
+  image: "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM"
+  },
+  {
+  name: "kimbap",
+  image: "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM"
+  },
+  {
+  name: "samgyetang",
+  image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+  }
+]  
 
 function App() {
   return (
     <div>
-      <h1>i am in App@!!</h1>
-      <Food fav="kimchi" /> 
-      <Food fav="kimbab" /> 
-      <Food fav="hamburger" /> 
-      <Food fav="mando" /> 
-
+      <h1>i am in App!!</h1>
+      {foodILike.map((item) => <Food name={item.name} image={item.image} /> )}
     </div>
   )
 }
