aa50d541f5c440e0452d13a55ed383378a877b26
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-18 22:34:52 +0900

    14.1 Building the Router : Route안에 exact={true}를 추가해줌으로써 라우터가 정확한 렌더링을 하게 된다

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App(){
   return (
     <HashRouter>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
+      {/* 방법1
+      <Route path="/home">
+        <h1>Home</h1>
+      </Route>
+      <Route path="/home/detail">
+        <h1>Detail</h1>
+      </Route>
+      <Route path="/about">
+        <h1>About</h1>
+      </Route> */}
+      <Route path="/home" component={Home} exact={true}/>
+      <Route path="/home/detail" component={Detail} exact={true}/>
+      <Route path="/about" component={About} exact={true}/>
     </HashRouter>
     
   );
---
+++ src/routes/Detail.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Detail(){
+    return(
+        <span>I am from Detail in Home</span>
+    );
+}
+
+export default Detail;
