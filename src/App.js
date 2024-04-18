f32dce60737d27a8f82cea8a463be89fc16c0f7a
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-18 22:41:27 +0900

    14.2 Building the Navigation: a태그로 href로 주소를 입력할 경우 [새로고침]되고 동작이 제대로 이뤄지지 않는다

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 
 function App(){
   return (
     <HashRouter>
-      {/* 방법1
-      <Route path="/home">
-        <h1>Home</h1>
-      </Route>
-      <Route path="/home/detail">
-        <h1>Detail</h1>
-      </Route>
-      <Route path="/about">
-        <h1>About</h1>
-      </Route> */}
-      <Route path="/home" component={Home} exact={true}/>
+      <Navigation />
+      <Route path="/" component={Home} exact={true}/>
       <Route path="/home/detail" component={Detail} exact={true}/>
       <Route path="/about" component={About} exact={true}/>
     </HashRouter>
