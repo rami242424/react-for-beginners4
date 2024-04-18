26a3507007d3f4d0a32184360d4bcb9c0b2c41c7
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-18 17:20:01 +0900

    12.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component{
   state = {
@@ -6,11 +7,18 @@ class App extends React.Component{
     movies: [],
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 1000);
+  // async => getMovies 함수가 비동기라고 표시해줌
+  // getMovies는 api로 데이터를 받아오느라 시간이 좀 소요되니, 기다려라!
+  // await => axios가 끝날때 까지 기다렸다가 계속하렴!
+  // async & await : javaScript에게 "getMovies 함수에게 조금 시간이 필요하고, 우리는 그걸 기다려야한다!"" 라고 말하는 것
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
   }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
   render() {
     const { isLoading } = this.state;
     return (
