c3ca3447559d047f1c2955c6738192e9921ecbdb
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-18 16:34:56 +0900

    11.2 Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
-// function component는 function이며, return 후 screen에 표시
-// class component는 class이며, React.Component로 부터 확장되며, render method안에 넣어야, screen에 표시
-// class component 를 사용해야 되는 이유 => state
-// setState : state를 업데이트하고, render function을 호출한다 (렌더링 한다) => setState를 써야하는 이유
-// -------------------------
-// 문신으로 새겨야 할 만큼 중요한 것!!
-// setState를 호출할 때마다 react는 새로운 state와 함께 render function을 호출한다.
-
 import React from "react";
 
-// class => javascript 
 class App extends React.Component{
-  // state 는 object이며, component의 data를 넣을 공간이 있고, 데이터는 변한다.
-  // state : 바꾸고 싶은 data를 넣는 곳
-  // 아래의 코드와 작업들은 component의 data를 바꾸기 위해서 하고 있다.
+  // constructor 제일 먼저 동작
+  // constructor(props){
+  //   super(props);
+  //   console.log("hello i am a constructor");
+  // }
   state = {
     count : 0
   };
 
-  // add function
   add = () => {
-    // console.log("I am doing add");
-    // this.state.count = 1; // 이렇게 직접 state를 변경할 수 없다. 왜냐면 react가 render function을 refresh하지 않는다.
-    // this.setState({ count : 1 });
-    //  this.setState({ count : this.state.count + 1 }); // 이 방식으로 하면 state에 의존하게 되고 다른 곳에서의 업데이트로 인해 문제가 생길 수 있다.
     this.setState((current) => ({ count : current.count + 1 }));
-
   }
 
-  // minus function
   minus = () => {
-    // console.log("I am doing minus");
-    // this.state.count = -1; // 이렇게 직접 state를 변경할 수 없다. 왜냐면 react가 render function을 refresh하지 않는다.
-    // this.setState({ count : -1 });
-    // this.setState({ count : this.state.count - 1 }); // 이 방식으로 하면 state에 의존하게 되고 다른 곳에서의 업데이트로 인해 문제가 생길 수 있다.
-    this.setState((current) => ({ count : current.count - 1 })); // this.state => current로 바꿔주기
+    this.setState((current) => ({ count : current.count - 1 })); 
   }
 
+  componentDidMount(){
+    console.log("this is from componentDidMount");
+  }
 
+  componentDidUpdate(){
+    console.log("this is from componentDidUpdate");
+  }
 
+  componentWillUnmount(){
+    console.log("This is from componentWillUnmount, bye!");
+  }
 
   render() {
+    console.log("I am rendering");
     return (
     <div>
       <h1>The number is : {this.state.count} </h1>
@@ -52,3 +44,15 @@ class App extends React.Component{
 
 export default App;
 
+// mounting (컴포넌트가 mount될 때 & screen에 표시될 떄 & website에 갈 떄)
+// - constructor() : 자바스크립트에서 class를 만들 때 호출 (제일먼저 렌더링)
+// - render()
+// - componentDidMount() : "component가 처음 render 됐어" 라고 알려줌. 즉, render 후 동작 
+
+
+// updating : state를 변경할 때 
+// - render()
+// - componentDidUpdate() : state를 업데이트 하면, render 후 동작
+
+
+// unmounting : change the page 즉, component가 죽었을때 (사용하지않을떄)
--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import { createRoot } from 'react-dom';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+  </>
 );
