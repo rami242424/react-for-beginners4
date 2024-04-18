5c3a13be2c1200588ab9ca4dee8b36c4fcf6a85d
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-18 16:59:40 +0900

    11.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,24 @@
 import React from "react";
 
 class App extends React.Component{
-  // constructor 제일 먼저 동작
-  // constructor(props){
-  //   super(props);
-  //   console.log("hello i am a constructor");
-  // }
   state = {
-    count : 0
-  };
-
-  add = () => {
-    this.setState((current) => ({ count : current.count + 1 }));
+    isLoading: true,
+    movies: [],
   }
 
-  minus = () => {
-    this.setState((current) => ({ count : current.count - 1 })); 
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 1000);
   }
-
-  componentDidMount(){
-    console.log("this is from componentDidMount");
-  }
-
-  componentDidUpdate(){
-    console.log("this is from componentDidUpdate");
-  }
-
-  componentWillUnmount(){
-    console.log("This is from componentWillUnmount, bye!");
-  }
-
   render() {
-    console.log("I am rendering");
+    const { isLoading } = this.state;
     return (
     <div>
-      <h1>The number is : {this.state.count} </h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
+      {isLoading ? "Loading..." : "We are Ready!!" }
     </div> 
     );
   }
 }
 
 export default App;
-
-// mounting (컴포넌트가 mount될 때 & screen에 표시될 떄 & website에 갈 떄)
-// - constructor() : 자바스크립트에서 class를 만들 때 호출 (제일먼저 렌더링)
-// - render()
-// - componentDidMount() : "component가 처음 render 됐어" 라고 알려줌. 즉, render 후 동작 
-
-
-// updating : state를 변경할 때 
-// - render()
-// - componentDidUpdate() : state를 업데이트 하면, render 후 동작
-
-
-// unmounting : change the page 즉, component가 죽었을때 (사용하지않을떄)
