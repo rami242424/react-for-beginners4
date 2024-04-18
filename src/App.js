b4877c20d563e411d842498cfc5f3673e27d04d0
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-18 17:34:45 +0900

    12.1 Rendering the Movies -1

--- src/App.js
+++ src/App.js
@@ -12,7 +12,19 @@ class App extends React.Component{
   // await => axios가 끝날때 까지 기다렸다가 계속하렴!
   // async & await : javaScript에게 "getMovies 함수에게 조금 시간이 필요하고, 우리는 그걸 기다려야한다!"" 라고 말하는 것
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    // const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    // console.log(movies.data.data.movies);
+
+    // // ec6를 적용하여 더 간결하게 표현하면
+    // const { data: { data: { movies } } }  = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    // console.log(movies);
+
+    const { 
+      data: { 
+        data: { movies } 
+      } 
+    }  = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    console.log(movies);
   }
 
   componentDidMount() {
