40457a27ef0574efcaae8315ecb7f5d3ce1e1209
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-18 12:40:52 +0900

    10.1 JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from "react";
 
-function Food(props){ // props : fav, something, pepapa 다 들어있음 
-  
-  console.log(props);
-
+function Food({ fav }){ 
+  // console.log(props);
   return (
-    <h2>Food i love to eat</h2>
+    <h2>Food i love {fav}</h2>
   );
 }
 
 function App() {
   return (
     <div>
-      <h1>i am in App</h1>
-      <Food 
-        fav="kimchi" 
-        something={true}
-        pepapa={["hello", 1, 2, 3, true]}
-      /> 
-      {/* fav, something, pepapa 는 food 함수 컴포넌트에 인자(argument)로 넣는다 => props */}
-
+      <h1>i am in App@!!</h1>
+      <Food fav="kimchi" /> 
+      <Food fav="kimbab" /> 
+      <Food fav="hamburger" /> 
+      <Food fav="mando" /> 
 
     </div>
   )
