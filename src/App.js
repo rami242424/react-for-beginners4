288b9d5189ffb2c617576e011102448cf20d9cc2
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-18 12:08:54 +0900

    10.1 JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import { 
-  BrowserRouter as Router,
-  Switch, // Route 찾는 용도, 한번에 하나의 Route만 렌더링 하기 위해서
-  Route, // url
-  Link, // 브라우저 새로고침 없이도 유저를 다른 페이지로 이동시켜주는 컴포넌트
-  } from "react-router-dom";
-  import Home from "./routes/Home";
-  import Detail from "./routes/Detail";
+import React from "react";
 
+function Food(){
+  return (
+    <h2>Food i love to eat</h2>
+  );
+}
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/hello">
-          <h1>oh hello~~</h1>
-        </Route>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/"> {/* component 적는 곳*/}
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
-  );
+    <div>
+      <h1>i am in App</h1>
+      <Food name="kimchi" />
+      <div className="hello"></div>
+    </div>
+  )
 }
 
 export default App;
