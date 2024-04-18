c7f83c66e416633d7f3acff7c89be8180b9b4788
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-18 15:52:28 +0900

    11.0 Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,62 +1,39 @@
+// function component는 function이며, return 후 screen에 표시
+// class component는 class이며, React.Component로 부터 확장되며, render method안에 넣어야, screen에 표시
+// class component 를 사용해야 되는 이유 => state
 import React from "react";
 
-import PropTypes from "prop-types";
+// class => javascript 
+class App extends React.Component{
+  // state 는 object이며, component의 data를 넣을 공간이 있고, 데이터는 변한다.
+  // state : 바꾸고 싶은 data를 넣는 곳
+  // 아래의 코드와 작업들은 component의 data를 바꾸기 위해서 하고 있다.
+  state = {
+    count : 0
+  };
 
-function Food({ name, image, rating }){
-  return (
-    <div>
-      <h2>i love { name }</h2>
-      <h3> 평점 { rating } / 10</h3>
-      <img src={ image } alt= { name } />
-    </div>
-  );
-}
-
-
-Food.propTypes ={
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.string.isRequired,
-}
-
-
-const foodILike = [
-  {
-    id:1,
-    // rating: 9,
-  name: "Kimchi",
-  image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
-  },
-  {
-    id:2,
-    rating: 9.5,
-  name: "bulgogi",
-  image: "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM"
-  },
-  {
-    id:3,
-    rating: 7,
-  name: "kimbap",
-  image: "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM"
-  },
-  {
-    id:4,
-    rating: 10,
-  name: "samgyetang",
-  image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+  // add function
+  add = () => {
+    console.log("I am doing add");
   }
-]  
 
-function App() {
-  return (
+  // minus function
+  minus = () => {
+    console.log("I am doing minus");
+  }
+
+
+
+
+  render() {
+    return (
     <div>
-      {foodILike.map((item) => (
-        <Food key={item.id}name={item.name} image={item.image} rating={item.rating} />
-      ))}
-    </div>
-  )
+      <h1>The number is : {this.state.count} </h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div> 
+    );
+  }
 }
-
-
 
 export default App;
