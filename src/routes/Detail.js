a164032cbb60f7e0881f2c0f1d2c0a8f22d5b8fe
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-19 00:03:36 +0900

    14.4강 콘솔창에러로 니꼴버전 복사전 마지막'

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,10 +1,25 @@
 import React from "react";
 
-function Detail(props){
-    console.log(props)
-    return(
-        <span>I am from Detail in Home</span>
-    );
+// 영화 클릭없이 직접 movie_detail을 입력하여 들어가면, 콘솔에서 state가 undefined로 뜬다. => 클릭한게 없으니 영화를 특정하지 않아서 정보를 받아 올 수 없음.
+// function Detail({location}){
+//     console.log(location);
+//     return(
+//         <span>I am from Detail in Home</span>
+//     );
+// }
+
+class Detail extends React.Component{
+    componentDidMount(){
+        const { location } = this.props;
+        console.log(location.state);
+    }
+
+
+    render(){
+        return (
+            <span>I am from Detail in Home</span>
+        );
+    }
 }
 
 export default Detail;
